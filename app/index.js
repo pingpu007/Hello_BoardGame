8f626952de89296035ed0ef3c6652dd77421139a
WoraponK <worapon.klabsri@gmail.com>
2024-04-16 12:31:50 +0700

    commit frontdev

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text } from 'react-native'
+
+const index = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>HEllo</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default index
