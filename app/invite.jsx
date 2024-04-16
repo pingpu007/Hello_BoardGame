fc8e6f84c4cac3853b95c4e94b124f0d3b93aa98
WoraponK <worapon.klabsri@gmail.com>
2024-04-16 15:44:02 +0700

    add header and fix layout

---
+++ app/invite.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text } from 'react-native'
+
+const InvitePage = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Invite Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default InvitePage
